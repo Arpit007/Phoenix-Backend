c4f6c994face59d3b90acd824a0a908a841a6d96
nkkumawat <nk0kumawat@gmail.com>
2018-01-13 18:24:11 +0530

    feedback routes

--- routes/event.js
+++ routes/event.js
@@ -20,13 +20,13 @@ router.post('/store_event', function (req, res) {
     return model.event.storeFeedback(name , sDate , eDate , description)
         .catch((e) => res.json(response(e)));
 });
-
-router.post('/event_by_id', function (req, res) {
-    "use strict";
-    const eventID = req.body.eventID;
-
-    return model.event.getEventByID(eventID)
-        .catch((e) => res.json(response(e)));
-});
+//
+// router.post('/event_by_id', function (req, res) {
+//     "use strict";
+//     const eventID = req.body.eventID;
+//
+//     return model.event.getEventByID(eventID)
+//         .catch((e) => res.json(response(e)));
+// });
 
 module.exports = router;
--- routes/feedback.js
+++ routes/feedback.js
@@ -11,16 +11,62 @@ const statusCode = require('../model/statusCode');
 const model = require('../model/model');
 
 
-router.post('/store_feedback', function (req, res) {
+router.post('/feedback', function (req, res) {
     "use strict";
-    const userId = req.body.userID;
-    const eventId = req.body.eventID;
+    const userID = req.body.userID;
+    const eventID = req.body.eventID;
     const description = req.body.description;
 
-
     return model.feedback.storeFeedback(userID , eventID , description)
-        .catch((e) => res.json(response(e)));
-
+        .catch((e) => res.json(response(e) ));
 });
+
+router.post('/getFeedbackByUserId', function (req, res) {
+    "use strict";
+    const userID = req.body.userID;
+    const eventID = req.body.eventID;
+    const description = req.body.description;
+
+    return model.feedback.getFeedbackByUser(userID)
+        .then((feedback) => {
+            let reply = response(statusCode.Ok);
+            reply.body.userID = feedback.userID.toString();
+            reply.body.eventID = feedback.eventID.toString();
+            reply.body.description = feedback.desc.toString();
+            res.json(reply);
+        })
+        .catch((e) => res.json(response(e)));
+});
+
+router.post('/getFeedbackByEvent', function (req, res) {
+    "use strict";
+    const eventID = req.body.eventID;
+    return model.feedback.getFeedbackByEvent(userID)
+        .then((feedback) => {
+            let reply = response(statusCode.Ok);
+            reply.body.userID = feedback.userID.toString();
+            reply.body.eventID = feedback.eventID.toString();
+            reply.body.description = feedback.desc.toString();
+            res.json(reply);
+        })
+        .catch((e) => res.json(response(e)));
+});
+
+router.post('/getFeedbackByUserAndEvent', function (req, res) {
+    "use strict";
+    const userID = req.body.userID;
+    const eventID = req.body.eventID;
+    return model.feedback.getFeedbackByUserAndEvent(userID , eventID)
+        .then((feedback) => {
+            let reply = response(statusCode.Ok);
+            reply.body.userID = feedback.userID.toString();
+            reply.body.eventID = feedback.eventID.toString();
+            reply.body.description = feedback.desc.toString();
+            res.json(reply);
+        })
+        .catch((e) => res.json(response(e)));
+});
+
+
 
 module.exports = router;
