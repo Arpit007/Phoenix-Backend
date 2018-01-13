dd846dc79b3bd29891fbbb1cb92b305c3dbb60fe
Arpit007 <arpit2011@live.com>
2018-01-13 14:40:36 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const cors = require('cors');
+const logger = require('morgan');
+const helmet = require('helmet');
+const express = require('express');
+const flash = require('express-flash');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const compression = require('compression');
+const cookieParser = require('cookie-parser');
+
+const index = require('./routes/index');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(session({
+    secret : xConfig.crypto.SessionKey,
+    resave : false,
+    saveUninitialized : true,
+    cookie : { secure : true }
+}));
+app.use(flash());
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+
+app.use(function (req, res, next) {
+    const err = new Error('Not Found ' + req.originalUrl);
+    err.status = 404;
+    next(err);
+});
+
+app.use(function (err, req, res) {
+    res.locals.message = err.message;
+    res.locals.baseResponse = req.app.get('NODE_ENV') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
---
+++ src/ip.js
@@ -0,0 +1,15 @@
+/**
+ * Created by StarkX on 13-Jan-18.
+ */
+const os = require('os');
+
+module.exports = (port)=>{
+    "use strict";
+    let address = "";
+    const iFaces = os.networkInterfaces();
+    
+    for (let dev in iFaces) {
+        iFaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false ? address = details.address: undefined);
+    }
+    return address.toString() + ":" + port;
+};
